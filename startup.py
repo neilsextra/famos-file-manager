ac2b851a76f28fb5975b379c9f8113fb7ce43db6
Neil Brittliff <nebrittl@microsoft.com>
2018-12-03 16:33:38 +1100

    Added web.config

--- startup.py
+++ startup.py
@@ -9,4 +9,19 @@ startup.py, that just imports the app object. You can then just specify
 startup:app in the Gunicorn command.
 """
 
+from os import environ
 from famos_file_manager.webapp import app
+
+if __name__ == '__main__':
+
+    HOST = environ.get('SERVER_HOST', 'localhost')
+
+    try:
+
+        PORT = int(environ.get('SERVER_PORT', '5555'))
+
+    except ValueError:
+
+        PORT = 5555
+
+    app.run(HOST, PORT)
